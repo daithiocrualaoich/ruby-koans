aedff53569fc4347a9095b764d4ddc347b53b61a
Daithi O Crualaoich <daithi.ocrualaoich@gmail.com>
2012-04-09 21:38:53 +0100

    Complete constants, control statements, exceptions, iteration, triangle and truth value koans.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # Raise error on nontriangles
+  ordered = [a, b, c].sort
+  if ordered[0] <= 0
+    raise TriangleError, "Triangles cannot have zero or negative length sides"
+  end
+  
+  if ordered[0] + ordered[1] <= ordered[2]
+    raise TriangleError, "Triangles must be closed"  
+  end
+
+  [:equilateral, :isosceles, :scalene][[a, b, c].uniq.length - 1]
 end
 
 # Error class used in part 2.  No need to change this code.
